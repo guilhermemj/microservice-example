a75e9843e156bd74db6365ec980697ab4ddcaa39
Guilherme Jacomini <guilhermem.j@hotmail.com>
2020-02-07 18:16:47 -0300

    feat: Add WebServer

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
 import 'dotenv/config';
 import 'module-alias/register';
 
-console.log('Hello world');
+import WebServer from '@guilhermemj/micro-web-server';
+
+import routes from './routes';
+
+const {
+  SERVER_HTTP_PORT = '3000',
+} = process.env;
+
+const httpPort = Number.parseInt(SERVER_HTTP_PORT, 10);
+const webServer = new WebServer({ httpPort, routes });
+
+// Start application
+(async (): Promise<void> => {
+  await webServer.start();
+  console.log(`Web server started at port ${httpPort}`);
+})();
